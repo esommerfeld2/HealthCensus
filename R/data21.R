63da8f070207dc932087f4e0a00153bd5d8423ce
esommerfeld2 <esommerfeld@smith.edu>
2025-11-11 15:41:47 -0500

    a start on loading in the data

---
+++ R/data21.R
@@ -0,0 +1,9 @@
+#' Health Census Data for 2021
+#'
+#' descirption
+#'
+#' @format ## data21
+#' A data frame with n rows and n columns
+#'
+#' MORE on slides? Idk it was a in class example
+"data21"
