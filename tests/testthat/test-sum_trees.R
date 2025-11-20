abaae73119be0f97483bf301156218f161417bc3
CJXU27 <CJXU@SMITH.EDU>
2025-11-20 15:39:06 -0500

    readme.rmd

---
+++ tests/testthat/test-sum_trees.R
@@ -0,0 +1,5 @@
+test_that("sum_trees works", {
+  test_data <- data23(species = c("tsugca", "pinure", "querru", "piceab"), status = c(AU, A, DS))
+  # count by status
+  result_status <- sum_trees(test_data, "")
+})
