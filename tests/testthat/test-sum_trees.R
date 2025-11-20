0a8cd22435495dd7d314700f66961b6e3e93bc0d
CJXU27 <CJXU@SMITH.EDU>
2025-11-20 15:58:42 -0500

    test file sum trees update

--- tests/testthat/test-sum_trees.R
+++ tests/testthat/test-sum_trees.R
@@ -1,5 +1,7 @@
 test_that("sum_trees works", {
 #   test_data <- data23(species = c("tsugca", "pinure", "querru", "piceab"), status = c(AU, A, DS))
-#   # count by status
+#   ## count by status
 #   result_status <- sum_trees(test_data, "")
+   # expect_equal(result_status$n))
+   # expect_equal(result_status$Categorical))
  })
