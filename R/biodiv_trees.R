9df5ce78871423dc8c15e092503bf4d72fcc86b7
CJXU27 <CJXU@SMITH.EDU>
2025-12-16 02:26:16 -0500

    biodiversity function basically done but needs review if you guys have tiem!

--- R/biodiv_trees.R
+++ R/biodiv_trees.R
@@ -1,61 +1,72 @@
 #' Calculating Tree Biodiversity Index at the Quadrat Level as Measure of Tree Mortality
 #'
-#' @description Calculates tree biodiversity by quadrat using data on the species composition per quadrat. Merges the calculated biodiversity index values back into the full dataset. Specifically takes abundance of species at the quadrat level to compute biodiversity index values (Shannon index, effective number of species, species richness, maximum possible diversity, Pielou's evenness J) and appends these index values to each tree in the given quadrat. It then computes summaries of indices of tree crown health and mortality rate, allowing easy visualization of the relationship between a tree's biodiversity index and its mortality.
-#' @details The function computes the abundance of tree species per quadrat and creates a matrix out of these values. Then, based on the type of biodiversity index that the user selects, it calculates that index for each quadrat and merges this biodiversity index back into the original dataset (data). It then computes the averages of percentage_crown_living and percentage_crown_intact as measures of tree health and mortality at the quadrat level.
+#' @description Calculates tree biodiversity by quadrat as a function of tree mortality rate in a given quadrat using data on the species composition per quadrat, and merges the calculated biodiversity index values and mortality rate values back into the full dataset.
+#' Specifically takes abundance of species at the quadrat level to compute biodiversity index values (Shannon index, effective number of species, species richness, maximum possible diversity, Pielou's evenness J) and appends these index values to each tree in the given quadrat.
+#' @details Computes the abundance of tree species per quadrat based on the number of trees per species in each quadrat.
+#' Then, it calculates the biodiversity indices mentioned in the description for each quadrat using functions from the 'vegan' package and merges these values back into the original dataset.
+#' It also computes tree mortality rate per quadrat given the proportion of dead trees (as indicated by a value of either 'DN', 'DS', or 'DC' for the 'status' variable) compared to the total number of trees in that quadrat.
 #' @param data A data frame containing tree-level census data from a given year from 2021-2024.
-#' @param index A character string specifying what kind of biodiversity index to calculate (shannon, simpson, or species richness).
 #'
-#' @return A data frame that appends the biodiversity index for a given quadrat to the original data frame. Also includes new columns for the mean percentage_crown_living and mean percentage_crown_intact biodiversity scores per quadrat.
+#' @return A data frame that appends various biodiversity indices and the tree mortality rate for a given quadrat to the original data frame.
 #' @export
 #' @import vegan
+#' @import dplyr
+#' @import tidyverse
 #' @examples
-#' biodiv_trees(data24, index = "shannon")
+#' biodiv_trees(data24)
 #'
-biodiv_trees <- function(data,
-                        index = "shannon") {
-  #To get rid of warnings about our parameters
-  data2 <-data
-  index2 <- index
-  vegan::diversity(c(1, 2, 3))
-  #
-  # if (!requireNamespace("dplyr")) stop("Package 'dplyr' required.")
-  # if (!requireNamespace("tidyr")) stop("Package 'tidyr' required.")
-  # if (!requireNamespace("vegan")) stop("Package 'vegan' required.")
-  #
-  # # create matrix of abundance of species per quad
-  # species_matrix <- data |>
-  #   group_by(quadrat, species) |>
-  #   summarise(n = n(), .groups = "drop") |>
-  #   pivot_wider(names_from = species, values_from = n, values_fill = 0)
-  #
-  # quad_ids <- species_matrix$quadrat
-  # species_only <- species_matrix[, -1]
-  #
-  # # select biodiversity index
-  # if (index == "shannon") {
-  #   biodiv <- diversity(species_only, index = "shannon")
-  # } else if (index == "simpson") {
-  #   biodiv <- diversity(species_only, index = "simpson")
-  # } else if (index == "species") {
-  #   biodiv <- specnumber(species_only)
-  # } else {
-  #   stop("Index must be one of: 'shannon', 'simpson', 'species'")
-  # }
-  #
-  # biodiv_df <- data.frame(
-  #   quadrat = quad_ids,
-  #   biodiversity = biodiv
-  # )
-  #
-  # # merge index calculation back to original dataset
-  # merge <- data |>
-  #   left_join(biodiv_df, by = "quadrat") |>
-  #   group_by(quadrat) |>
-  #   mutate(
-  #     mean_crown_living_quadrat = mean(percentage_of_crown_living, na.rm = TRUE),
-  #     mean_crown_intact_quadrat = mean(percentage_of_crown_intact, na.rm = TRUE)
-  #   ) |>
-  #   ungroup()
-  #
-  # return(merge)
+biodiv_trees <- function(data) {
+# remove missing species values from data, make sure each row/tree (n()) per species is summarized by quad_sub_quad (quadrat)
+bio.long <- data |>
+  filter(!is.na(species)) |>
+  select(quad_sub_quad) |>
+  mutate(ifelse(dead == "DS", "DN", "DC")) |>
+  group_by(quad_sub_quad, species) |>
+  summarize(abundance = n())
+
+# convert to wide format
+bio.wide <- bio.long |>
+  pivot_wider(names_from = species,
+              values_from = abundance,
+              values_fill = list(abundance = 0)) |>
+  column_to_rownames("quad_sub_quad")
+
+# calculate diversity index values
+
+# shannon index (H)
+shannon <- diversity(bio.wide)
+
+# effective number of species
+effective.sp <- exp(shannon)
+
+# species richness
+richness <- specnumber(bio.wide)
+
+# maximum possible diversity
+max.H <- log(richness)
+
+# Pielou's evenness J
+evenness.J <- shannon / max.H
+
+# create data set with all diversity index values
+bio.index <- data.frame(shannon, effective.sp, richness, max.H, evenness.J) |>
+  rownames_to_column("quad_sub_quad")
+
+# create a data set for mortality per quadrat
+mortality <- bio.long |>
+  group_by(quad_sub_quad) |>
+  summarize(total_trees = n(),
+            dead_trees = sum(dead),
+            mortality_rate = dead_trees / total_trees)
+# select quad_sub_quad to preserve quadrat/location metadata in final dataset
+quadrat <- data |>
+  select(quad_sub_quad) |>
+  distinct()
+
+# join bio.index to original data set
+# quad_sub_quad is the "key" used to match up the two datasets
+merged.df <- left_join(bio.index, mortality, by = "quadrat")
+# return merged data frame with biodiversity indices and mortality per quadrat merged to original dataset
+return(merged.df)
 }
+
